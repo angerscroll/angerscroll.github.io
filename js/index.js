de2703dfc883e50a2b5a62fb29bbb1532616549d
angerscroll <angerscroll@gmail.com>
2023-01-13 11:59:09 +0900

    test5

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
-let app = document.getElementById("app");
+import Game from "./game/Game.js";
+import GameView from "./game/GameView.js";
 
-app.innerHTML= '<div class="letter">hi hello 효정</div>';
+
+
+console.log('index.js')
+
+const row = 5;
+const cell =5;
+const bn  = `1111100000111110000011111`;
+
+const gameStart  = new Game({row,cell});
+let gameView = new GameView(document.getElementById("app"));
+
+gameView.onTileClick = i => {
+  console.log(`Tile clicked : ${i}`);
+}
