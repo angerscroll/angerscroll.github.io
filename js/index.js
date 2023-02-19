e9bb419fb569c5d9db24d20c1cc9dd0d33aaf2c4
angerscroll <angerscroll@gmail.com>
2023-02-19 11:32:59 +0900

    nav add

--- js/index.js
+++ js/index.js
@@ -1,10 +1,45 @@
 const app = document.getElementById('app');
 
-function render() {
+const render = () => {
   app.innerHTML = `
     <h1>Welcome to My Blog!</h1>
     <p>This is a sample SPA page using vanilla JavaScript.</p>
   `;
 }
+
+const router = () => {
+  const path = location.hash.substring(1);
+  switch (path) {
+    case '/':
+      app.innerHTML = home();
+      setActiveLink('/');
+      break;
+    case '/post':
+      app.innerHTML = post();
+      setActiveLink('/post');
+      break;
+    case '/about':
+      app.innerHTML = about();
+      setActiveLink('/about');
+      break;
+    default:
+      app.innerHTML = `
+        <h1>404 Not Found</h1>
+        <p>The requested URL ${path} was not found on this server.</p>
+      `;
+  }
+}
+
+const setActiveLink = (path) => {
+  const links = document.querySelectorAll('nav a');
+  links.forEach(link => {
+    if (link.getAttribute('href') === `#${path}`) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+}
+
 
 render();
